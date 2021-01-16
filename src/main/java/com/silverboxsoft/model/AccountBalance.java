020b1ca1640831902c401c2abc0554cc0fad8690
Eiji Tanaka <tanaka.eiji@nifty.com>
2021-01-16 21:04:37 +0900

    remove unused comment

--- src/main/java/com/silverboxsoft/model/AccountBalance.java
+++ src/main/java/com/silverboxsoft/model/AccountBalance.java
@@ -5,13 +5,10 @@ import lombok.Data;
 @Data
 public class AccountBalance {
 
-	// @JsonProperty("tgt_date")
 	private String tgtDate;
 
-	// @JsonProperty("method_cd")
 	private String methodCd;
 
-	// @JsonProperty("value")
 	private Long value;
 
 }
