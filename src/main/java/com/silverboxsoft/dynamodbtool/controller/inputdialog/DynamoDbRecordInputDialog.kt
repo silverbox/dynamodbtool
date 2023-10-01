2ce74765ef5af0ce8ae106809eb3363c32c6ce94
Eiji Tanaka <tanaka.eiji@nifty.com>
2023-10-01 16:44:04 +0900

    fix attribute sort issue

--- src/main/java/com/silverboxsoft/dynamodbtool/controller/inputdialog/DynamoDbRecordInputDialog.kt
+++ src/main/java/com/silverboxsoft/dynamodbtool/controller/inputdialog/DynamoDbRecordInputDialog.kt
@@ -17,6 +17,8 @@ class DynamoDbRecordInputDialog(tableInfo: TableDescription, dynamoDbRecord: Map
                                 private val editMode: DynamoDbEditMode
 ) : DynamoDbMapInputDialog(dynamoDbRecord, "") {
     private var tblStructColumnList: List<DynamoDbColumn> = ArrayList()
+    private var hashKeyName: String = ""
+    private var sortKeyName: String = ""
     private val keyColumnSet: MutableSet<String> = HashSet()
     private val requireColumnSet: MutableSet<String> = HashSet()
     // add key info first
@@ -29,29 +31,35 @@ class DynamoDbRecordInputDialog(tableInfo: TableDescription, dynamoDbRecord: Map
             if (tblStructColumnList == null) {
                 return retList
             }
-            if (attrNameList == null) {
-                attrNameList = ArrayList()
-            }
 
-            // add key info first
-            for (colIdx in tblStructColumnList.indices) {
-                val attrName = tblStructColumnList[colIdx].columnName
+            fun addAttr(attrName: String) {
                 val wkAttr = dynamoDbRecordOrg.getOrDefault(attrName, AbsDynamoDbInputDialog.NULL_ATTRIBUTE)
                 retList.add(getOneBodyAttributeNodeList(attrName, wkAttr))
                 attrNameList.add(attrName)
             }
 
-            // pick up the data which attribute name is come yet.
-            // Set<String> unsetKeyNameSet = getDynamoDbRecordOrg().keySet();
-            val allAttrNameList: List<String> = DynamoDbUtils.Companion.getSortedAttrNameList(dynamoDbRecordOrg)
-            val keyNameSet = attrNameList.stream().collect(Collectors.toSet())
-            for (newAttrName in allAttrNameList) {
-                if (keyNameSet.contains(newAttrName)) {
+            // add key info first
+            addAttr(hashKeyName)
+            if (sortKeyName.isNotEmpty()) {
+                addAttr(sortKeyName)
+            }
+
+            // add index column
+            for (newAttrName in requireColumnSet.toList()) {
+                if (attrNameList.contains(newAttrName)) {
                     continue
                 }
-                val wkAttr = dynamoDbRecordOrg.getOrDefault(newAttrName, AbsDynamoDbInputDialog.NULL_ATTRIBUTE)
-                retList.add(getOneBodyAttributeNodeList(newAttrName, wkAttr))
-                attrNameList.add(newAttrName)
+                addAttr(newAttrName)
+            }
+
+            // pick up the data which attribute name is not come yet.
+            // Set<String> unsetKeyNameSet = getDynamoDbRecordOrg().keySet();
+            val allAttrNameList: List<String> = DynamoDbUtils.Companion.getSortedAttrNameList(dynamoDbRecordOrg)
+            for (newAttrName in allAttrNameList) {
+                if (attrNameList.contains(newAttrName)) {
+                    continue
+                }
+                addAttr(newAttrName)
             }
             return retList
         }
@@ -102,8 +110,14 @@ class DynamoDbRecordInputDialog(tableInfo: TableDescription, dynamoDbRecord: Map
         tblStructColumnList = DynamoDbUtils.Companion.getSortedDynamoDbColumnList(tableInfo)
         val keyInfoList = tableInfo.keySchema()
         for (elem in keyInfoList) {
-            keyColumnSet.add(elem.attributeName())
-            requireColumnSet.add(elem.attributeName())
+            val attrName = elem.attributeName()
+            keyColumnSet.add(attrName)
+            requireColumnSet.add(attrName)
+            if (elem.keyType().equals(KeyType.HASH)) {
+                this.hashKeyName = attrName
+            } else if (elem.keyType().equals(KeyType.RANGE)) {
+                this.sortKeyName = attrName
+            }
         }
         tableInfo.globalSecondaryIndexes().stream().forEach {
             gsiDesc: GlobalSecondaryIndexDescription -> gsiDesc.keySchema().stream().forEach {
